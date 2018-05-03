623fdb588617724a7351d18858c02339e0dc9589
Jorge <mariocast09@gmail.com>
2018-05-02 20:55:47 -0600

    agrego hoja MOVIMIENTOS y modifico transaction html, ts y app.scss

---
+++ src/pages/transactions/transactions.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the TransactionsPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-transactions',
+  templateUrl: 'transactions.html',
+})
+export class TransactionsPage {
+
+  title : string = "Movimientos";
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad TransactionsPage');
+  }
+
+}
